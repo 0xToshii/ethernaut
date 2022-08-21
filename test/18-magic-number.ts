b899f063362b1bba904a6e392f9640af846f34f4
0xToshii <94262432+0xToshii@users.noreply.github.com>
2022-08-20 18:46:18 -0700

    magic number

--- test/18-magic-number.ts
+++ test/18-magic-number.ts
@@ -20,6 +20,28 @@ before(async () => {
 });
 
 it("solves the challenge", async function () {
+
+  let abi = [{"inputs":[], "name":"whatIsTheMeaningOfLife" , "outputs":[{ "internalType": "uint256","name": "","type": "address"}], "type":"function", "stateMutability": "pure"}]
+
+  // -- init code + runtime code + metadata
+  // -- init code: set code size to be 0a before CODECOPY, as there are 10 opcodes used
+  // -- runtime code:
+  // PUSH1 0x2a // 60 2a // number 42
+  // PUSH1 0x80 // 60 80 // first location of free memory
+  // MSTORE     // 52    // storing 42 in memory
+  // PUSH1 0x20 // 60 20 // size of uint256 return value
+  // PUSH1 0x80 // 60 80 // location of answer in memory
+  // RETURN     // f3    // return answer from memory
+
+  let bytecode = "0x6080604052348015600f57600080fd5b50600a8061001e6000396000f3fe" + "602a60805260206080f3" + "fea26469706673582212207df7101e891215df9a443ec6909b6c867ed0ec7eea747b66ceed64e4d397ee2764736f6c63430006000033"
+
+  const magicNumberFactory = await ethers.getContractFactory(abi,bytecode);
+  let magicNumberContract = await magicNumberFactory.connect(eoa).deploy()
+  
+  console.log(await magicNumberContract.whatIsTheMeaningOfLife())
+
+  await challenge.connect(eoa).setSolver(magicNumberContract.address)
+
 });
 
 after(async () => {
